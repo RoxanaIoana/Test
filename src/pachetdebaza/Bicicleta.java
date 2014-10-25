2026ee8d92d31c9246a64d380ab9dc459c56af64
RoxanaIoana <roxana.ioana92@gmail.com>
2014-10-25 18:19:28 +0300

    functions

---
+++ src/pachetdebaza/Bicicleta.java
@@ -0,0 +1,19 @@
+package pachetdebaza;
+
+/**
+ * Created by roxy on 10/25/2014.
+ */
+public class Bicicleta {
+
+    // membrii clasei care definesc proprietatile unei biciclete
+    int gear;
+    int cadence;
+    int speed;
+
+
+    public void setGear(int value) {
+
+        gear = value;
+    }
+
+}
---
+++ src/pachetdebaza/Main.java
@@ -0,0 +1,19 @@
+package pachetdebaza;
+
+/**
+ * Created by roxy on 10/25/2014.
+ */
+public class Main {
+
+    public static void main(String[] arguments) {
+
+        Bicicleta MountainBike = new Bicicleta();
+
+        MountainBike.setGear(20);
+
+        System.out.println(MountainBike.gear);
+
+
+
+    }
+}
